86dca1fb632122a748167f7cac91f62f1ef4b2b0
Simon Berner <sibebzh@gmail.com>
2023-12-21 14:37:55 +0100

    refactor: :recycle: refactor solution code for day2-part1
    TDD step 3: by refactoring the current solution, we can increase code readability, ensuring single responsibility and easier unit testing.

--- day02/PuzzleDay2.js
+++ day02/PuzzleDay2.js
@@ -1,5 +1,4 @@
 import {getInput} from "../utils/index.js";
-import allPossibleGameIds from "jshint/src/name-stack.js";
 
 class PuzzleDay2 {
     constructor() {
@@ -14,6 +13,7 @@ class PuzzleDay2 {
 
     getPossibleGames(recordInput) {
         let allPossibleGameIds = [];
+        // treat every single record entry (single line)
         recordInput.trim().split('\n').forEach(str => {
             console.log(`Part1 - rawRecordInput: ${str}`);
             // eg string: Game 1: 1 red, 3 blue, 11 green; 1 blue, 5 red; 3 blue, 5 green, 13 red; 6 red, 1 blue, 4 green; 16 red, 12 green
@@ -22,30 +22,27 @@ class PuzzleDay2 {
             // extract the Number from the gameId
             const gameIdNumber = parseInt(gameId.match(/\d+/)[0]);
             console.log(`Part1 - gameIdNumber: ${gameIdNumber}`);
-            // remove 'Game 1:' from the str
-            const sets = str.replace(gameId + ':', '');
-            // split the sets with ';' into single sets and put them in an array
-            const setsArray = sets.split(';');
-            let isPossible = true;
-            for (let set of setsArray) {
-                const cubes = set.trim().split(', ');
-                for (let cube of cubes) {
-                    const [count, color] = cube.split(' ');
-                    if (!this.givenCubes.has(color) || this.givenCubes.get(color) < count) {
-                        isPossible = false;
-                        break;
-                    }
-                }
-                if (!isPossible) {
-                    break;
-                }
-            }
-            // if all the sets are possible, add the game id as Number to allPossibleGameIds
-            if (isPossible) {
-                allPossibleGameIds.push(gameIdNumber);
+            // remove 'Game 1:' from the str and split the sets with ';' into single sets and put them in an array
+            const sets = str.replace(gameId + ':', '').split(';')
+            if (this.isGamePossible(sets)) {
+                allPossibleGameIds.push(gameIdNumber)
             }
         })
         return allPossibleGameIds;
+    }
+
+    isGamePossible(sets) {
+        const setsArray = sets;
+        for (const set of setsArray) {
+            const cubes = set.trim().split(', ');
+            for (const cube of cubes) {
+                const [count, color] = cube.split(' ');
+                if (!this.givenCubes.has(color) || this.givenCubes.get(color) < count) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     getSumOfGameIds(intValues) {
@@ -53,7 +50,6 @@ class PuzzleDay2 {
         return intValues.reduce(function (accumulator, currentValue) {
             return accumulator + currentValue
         });
-
     }
 }
 
