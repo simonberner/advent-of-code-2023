66c7b0bc8ec2d400a1ebffcbfa7254201a8bd877
Simon Berner <sibebzh@gmail.com>
2023-12-21 16:05:34 +0100

    test: :white_check_mark: add new tests for day2-part1
    Add some additional tests for covering the refactored solution code from the last commit.

--- day02/puzzle.test.js
+++ day02/puzzle.test.js
@@ -7,6 +7,8 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 `;
+const testSetPossible = [ "3 blue, 4 red", "1 red, 2 green, 6 blue", "2 green"];
+const testSetNotPossible = [ "8 green, 6 blue, 20 red", "5 blue, 4 red, 13 green", "5 green, 1 red"];
 const possibleGameIds = [1, 2, 5];
 const puzzleDay2 = new PuzzleDay2()
 
@@ -20,6 +22,18 @@ describe('getSumOfGameIds', () => {
 describe('getPossibleGames', () => {
     it('should return an Array of IDs of all the games which are possible.', () => {
         expect(puzzleDay2.getPossibleGames(exampleRecordInput)).toEqual(possibleGameIds);
+    });
+});
+
+describe('isGamePossible', () => {
+    it('should return true if a specific game is possible.', () => {
+        expect(puzzleDay2.isGamePossible(testSetPossible)).toEqual(true);
+    });
+});
+
+describe('isGamePossible', () => {
+    it('should return false if a specific game is NOT possible.', () => {
+        expect(puzzleDay2.isGamePossible(testSetNotPossible)).toEqual(false);
     });
 });
 
