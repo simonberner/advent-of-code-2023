93beda27c560e0bbd7296df5a4d5e19b997fe4b7
Simon Berner <sibebzh@gmail.com>
2023-12-31 18:06:26 +0100

    add: :sparkles: add starting code for day2-part2

--- day02/puzzle.js
+++ day02/puzzle.js
@@ -1,5 +1,10 @@
 import {PuzzleDay2} from "./PuzzleDay2.js";
 
+// Part1
 const puzzleDay2 = new PuzzleDay2();
 const possibleGames = puzzleDay2.getPossibleGames(puzzleDay2.textInput)
 console.log(`Solution #Day2-Part1: What is the sum of the IDs of those games? ${puzzleDay2.getSumOfGameIds(possibleGames)}`);
+
+// Part2
+const sumOfPowerOfAllGames = puzzleDay2.getSumOfPowerOfAllGames(puzzleDay2.textInput)
+console.log(`Solution #Day2-Part2: What is the sum of the power of these sets? ${sumOfPowerOfAllGames}`);
